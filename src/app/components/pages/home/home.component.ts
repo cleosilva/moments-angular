5b481f0582c41095314bd27eb9718c1d8e604a2e
Cleo Silva <cleo_silva1906@hotmail.com>
2022-09-01 10:02:45 -0300

    Requisição get na Api e criação da home para exibição

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  faSearch = faSearch;
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
-  constructor() { }
+  // todo search
+
+  constructor(private momentsService: MomentService) {}
 
   ngOnInit(): void {
-  }
+    this.momentsService.getMoments().subscribe((item) => {
+      const data = item.data;
 
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
